6e1666428a939f7085ff0b362cf5db76728b939b
huhaifeng <18296134761@163.com>
2016-04-01 12:37:59 +0800

    add demo

---
+++ SelectCircleDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SelectCircleDemo
+//
+//  Created by huhaifeng on 16/3/15.
+//  Copyright © 2016年 huhaifeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SelectCircleDemo/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  SelectCircleDemo
+//
+//  Created by huhaifeng on 16/3/15.
+//  Copyright © 2016年 huhaifeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@property (weak, nonatomic) IBOutlet UILabel *showLabel;
+@end
+
